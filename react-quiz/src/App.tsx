b180b9ab2a233988415225072429a8a54eef4af3
vogronicspatrik <vogronicspatrik@gmail.com>
2023-03-07 01:36:27 +0100

    fixed login issue

--- react-quiz/src/App.tsx
+++ react-quiz/src/App.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { auth } from './config/firebase';
+import logging from './config/logging';
 import { Route, Routes } from 'react-router-dom';
 import AuthRoute from './components/AuthRoute';
 import routes from './config/routes';
+import { Spinner } from 'reactstrap';
 
 export interface IApplicationProps { }
 
 const App: React.FunctionComponent<IApplicationProps> = props => {
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        auth.onAuthStateChanged(user => {
+            if (user)
+            {
+                logging.info('User detected.');
+            }
+            else
+            {
+                logging.info('No user detected');
+            }
+
+            setLoading(false);
+        })
+    }, []);
+
+    if (loading)
+        return <Spinner color="info" />
     return (
         <div>
             <Routes>
