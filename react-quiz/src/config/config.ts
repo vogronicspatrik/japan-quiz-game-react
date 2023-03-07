7f723ad2f1db5e2e9fb9dd50f048ad70d300e76e
vogronicspatrik <vogronicspatrik@gmail.com>
2023-03-07 11:32:49 +0100

    remove sensitive data

--- react-quiz/src/config/config.ts
+++ react-quiz/src/config/config.ts
@@ -1,12 +1,6 @@
 const config = {
     firebase: {
-        apiKey: "AIzaSyCbKmyJLT0OWk9pwcxSMiH4AT4M5kXSPVg",
-        authDomain: "gorin-login.firebaseapp.com",
-        projectId: "gorin-login",
-        storageBucket: "gorin-login.appspot.com",
-        messagingSenderId: "929913639264",
-        appId: "1:929913639264:web:d41d131c7d41ab371ece05",
-        measurementId: "G-J9TS8NWE2F"
+
     }
 }
 
