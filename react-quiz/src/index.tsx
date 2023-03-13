734eb4b9c261cc2db30df7d18cc40e64c47193a9
vogronicspatrik <vogronicspatrik@gmail.com>
2023-03-13 18:20:55 +0100

    pwa setup base

--- react-quiz/src/index.tsx
+++ react-quiz/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,3 +14,9 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
