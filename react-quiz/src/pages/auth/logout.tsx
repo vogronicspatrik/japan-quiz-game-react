20d8d0faaa7abde6dfc1c5a475cc86ff3cb6614a
vogronicspatrik <vogronicspatrik@gmail.com>
2023-04-04 01:34:31 +0200

    logout page translation

--- react-quiz/src/pages/auth/logout.tsx
+++ react-quiz/src/pages/auth/logout.tsx
@@ -1,14 +1,15 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import {  useNavigate } from 'react-router-dom';
-
 import AuthContainer from '../../components/AuthContainer';
 import { auth } from '../../config/firebase';
 import logging from '../../config/logging';
 import IPageProps from '../../interfaces/page';
-import { Typography, Button, createTheme, ThemeProvider, Container, MenuItem, Select } from '@mui/material';
+import { Typography, Button} from '@mui/material';
+import { useTranslation } from 'react-i18next';
 
 const LogoutPage: React.FunctionComponent<IPageProps> = props => {
     const navigate = useNavigate();
+    const {t} = useTranslation(["logout"]);
     const Logout = () => {
         auth.signOut()
         .then(() => navigate('/'))
@@ -16,16 +17,16 @@ const LogoutPage: React.FunctionComponent<IPageProps> = props => {
     }
 
     return (
-    <AuthContainer header="Logout">
+    <AuthContainer header={t("logout")}>
     <Typography variant="body1" align="center">
-        Are you sure you want to logout?
+        {t('sureLogout')}
     </Typography>
     <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
         <Button variant="contained" color="error" sx={{ marginRight: '10px' }} onClick={() => navigate(-1)}>
-        Cancel
+        {t('cancel')}
         </Button>
         <Button variant="contained" color="info" onClick={() => Logout()}>
-        Logout
+        {t('logout')}
         </Button>
     </div>
     </AuthContainer>
