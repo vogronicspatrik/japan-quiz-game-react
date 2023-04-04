7eeae184e0f4c70d2cf2045427c21cf05b150ab0
vogronicspatrik <vogronicspatrik@gmail.com>
2023-04-04 10:39:12 +0200

    score list ordered desc

--- react-quiz/src/pages/score.tsx
+++ react-quiz/src/pages/score.tsx
@@ -1,42 +1,43 @@
 import { makeStyles } from '@mui/styles';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import UserService from '../services/user-service';
 import { useEffect, useState } from 'react';
 
 const useStyles = makeStyles({
-    table: {
-      minWidth: 200,
-      maxWidth: 500,
-      margin: 'auto',
-    },
-  });
+  table: {
+    minWidth: 200,
+    maxWidth: 500,
+    margin: 'auto',
+  },
+});
 
 
 interface User {
-    name: string;
-    score: number;
-  }
+  name: string;
+  score: number;
+}
 
 
 const Score = () => {
   const classes = useStyles();
-  const {t} = useTranslation(["pages"]);
+  const { t } = useTranslation(["pages"]);
   const userService = new UserService();
   const [users, setUsers] = useState<User[]>([]);
 
   useEffect(() => {
     const getUsers = async () => {
-        const data = await userService.getUsers();
-        const updatedUsers = data.docs.map((doc) => ({name: doc.data().FirstName, score:doc.data().Points })) as User[];
-        setUsers(updatedUsers);
+      const data = await userService.getUsers();
+      const updatedUsers = data.docs.map((doc) => ({ name: doc.data().FirstName, score: doc.data().Points })) as User[];
+      const sortedUsers = [...updatedUsers].sort((a, b) => b.score - a.score);
+      setUsers(sortedUsers);
     };
 
     getUsers();
   }, []);
 
   return (
-<TableContainer sx={{paddingTop:"80px"}}>
+    <TableContainer sx={{ paddingTop: "80px" }}>
       <Table className={classes.table} aria-label="user scores">
         <TableHead>
           <TableRow>
